82bd72838aabdc3ebb5f1acdd8e5cdcdd8ad75c6
NihaalWagadia <nwagadia@gmail.com>
2020-08-08 18:18:31 -0400

    XMLPlay

--- app/src/main/java/com/example/quizzydizzy/PlayAct.kt
+++ app/src/main/java/com/example/quizzydizzy/PlayAct.kt
@@ -3,13 +3,74 @@ package com.example.quizzydizzy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_play.*
 
 class PlayAct : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play)
 
         val questionList = Constants.getQuestions()
         Log.i("Question List Size", "${questionList.size}")
+        val button1 = findViewById<Button>(R.id.one)
+        val button2 = findViewById<Button>(R.id.two)
+        val button3 = findViewById<Button>(R.id.three)
+        val button4 = findViewById<Button>(R.id.four)
+        val button5 = findViewById<Button>(R.id.five)
+        val button6 = findViewById<Button>(R.id.six)
+        val button7 = findViewById<Button>(R.id.seven)
+        val button8 = findViewById<Button>(R.id.eight)
+        val button9 = findViewById<Button>(R.id.nine)
+        val button0 = findViewById<Button>(R.id.zero)
+        val buttonClear = findViewById<Button>(R.id.clear)
+
+        button0.setOnClickListener {
+            showResult("0", true)
+        }
+        button1.setOnClickListener {
+            showResult("1", true)
+        }
+        button2.setOnClickListener {
+            showResult("2", true)
+        }
+        button3.setOnClickListener {
+            showResult("3", true)
+        }
+        button4.setOnClickListener {
+            showResult("4", true)
+        }
+        button5.setOnClickListener {
+            showResult("5", true)
+        }
+        button6.setOnClickListener {
+            showResult("6", true)
+        }
+        button7.setOnClickListener {
+            showResult("7", true)
+        }
+        button8.setOnClickListener {
+            showResult("8", true)
+        }
+        button9.setOnClickListener {
+            showResult("9", true)
+        }
+        buttonClear.setOnClickListener {
+            answer_input.text = ""
+        }
+
+
+
     }
+
+
+}
+
+fun showResult(string: String, canClear: Boolean) {
+    if (canClear) {
+        answer_input.append(string)
+    }
+}
 }
